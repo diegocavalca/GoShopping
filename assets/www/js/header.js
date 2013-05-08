515005562f273dbaaa3d64ec5185a8e241d89023
Diego Cavalca <diego.cavalca@ceteclins.com.br>
2013-05-08 14:37:14 -0300

    Source code GoShopping

---
+++ assets/www/js/header.js
@@ -0,0 +1,9 @@
+document.write('<div data-role="header" data-id="foo1" data-position="fixed">');
+	document.write('<div data-role="navbar">');
+		document.write('<ul>');
+			document.write('<li><a data-iconpos="top" data-icon="info">Informa&ccedil;&otilde;es</a></li>');
+			document.write('<li><a href="estacionamento.html" data-ajax="false" data-iconpos="top" data-icon="grid">Minha vaga</a></li>');
+			document.write('<li><a data-iconpos="top" data-icon="refresh">Sincronizar</a></li>');					
+		document.write('</ul>');
+	document.write('</div>');
+document.write('</div>');
